af4d3d86629a3355e5492d7029cb2f4902e59e39
Alexey Belyakovskiy <2697024@gmail.com>
2021-07-13 21:50:26 +0300

    Get lengthSeconds

--- src/Models/VideoDetails.php
+++ src/Models/VideoDetails.php
@@ -47,4 +47,9 @@ class VideoDetails
     {
         return Utils::arrayGet($this->videoDetails, 'viewCount');
     }
+
+    public function getLengthSeconds()
+    {
+        return Utils::arrayGet($this->videoDetails, 'lengthSeconds');
+    }
 }
